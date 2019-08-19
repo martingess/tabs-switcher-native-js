238f37afca664507aa357361623d054be63ab3bd
Denis <martingess@protonmail.ch>
2019-08-19 12:09:24 +0300

    tabs

---
+++ js/script.js
@@ -0,0 +1,33 @@
+    'useStrict';
+
+    let info = document.querySelector('.info-header'),
+        tabs = document.querySelectorAll('.info-header-tab'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabs.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove("hide");
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tabs.length; i++) {
+                if (target == tabs[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                }
+            }
+        }
+    });
